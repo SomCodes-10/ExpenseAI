70952a66de683982c91ee5aa2939296296441787
Somraj <somrajdeb100@gmail.com>
2026-07-04 19:20:58 +0530

    Initial mongoDB setup

--- server/app.js
+++ server/app.js
@@ -1,7 +1,29 @@
 import express from 'express';
+import dotenv from "dotenv";
+import connectDB from './config/db.js';
+import healthRouter from './routes/health.js';
+
+dotenv.config()
+
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log('Server is running on port 3000');
-});
+app.use(express.json())
+app.use('/api/health', healthRouter)
+
+// connectDB()
+
+const startServer = async () => {
+  try {
+    await connectDB()
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+
+  }catch(error){
+    console.log("Failed to start server:",error)
+  }
+}
+
+startServer();
