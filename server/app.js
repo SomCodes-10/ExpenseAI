df5d5c8e022b6ba53b58c55999dbb7b1de801e86
Somraj <somrajdeb100@gmail.com>
2026-07-17 18:55:58 +0530

    updated db pass

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ import aiRouter from './routes/ai.routes.js';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors({
   origin: "http://localhost:5173", 
