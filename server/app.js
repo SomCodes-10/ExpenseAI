ec3f89bf5d6ea4e133b8276bddde623408b50243
Somraj <somrajdeb100@gmail.com>
2026-07-17 20:03:44 +0530

    fiexd cors

--- server/app.js
+++ server/app.js
@@ -19,11 +19,11 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use('/api/health', healthRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/transactions',transactionRouter)
 app.use('/api/ai',aiRouter)
-app.use(cors(corsOptions));
 app.use(errorHandler);
 
 
